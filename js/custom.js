28854782d31bf7dde14f662c4703f9318fab2930
TeSkiBoi <terrencebriones111703@gmail.com>
2025-10-22 02:49:42 +0800

    commit stuff

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+function showAlertYesNo() {
+    let userResponse = confirm("Do you want to proceed?");
+    if (userResponse) {
+        alert("You clicked Yes!");
+    } else {
+        alert("You clicked No!");
+    }
+}
+
+function computeSum() {
+    let num1 = parseFloat()
+}
