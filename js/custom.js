6998c4a7f161bdbffcaaca29d5839039f80e5832
TeSkiBoi <terrencebriones111703@gmail.com>
2025-10-24 04:52:21 +0800

    commit

--- js/custom.js
+++ js/custom.js
@@ -12,17 +12,19 @@ function showAlertYesNo() {
 }
 
 function computeSum() {
-    let num1 = parseFloat(document.getElementById("num1").value);
-    let num2 = parseFloat(document.getElementById("num2").value);
+    let num1 = parseFloat(document.getElementById("number1").value);
+    let num2 = parseFloat(document.getElementById("number2").value);
     if (isNaN(num1) || isNaN(num2)) {
         alert("Please enter valid numbers.");
         return;
     }
-    let userResponse = confirm(`Do you want to compute the sum of ${num1} and ${num2}?`);
+    let userResponse = confirm("Do you want to continue?");
     if (userResponse) {
         let sum = num1 + num2;
         alert("The sum is: " + sum);
+
     } else {
-        alert("Operation cancelled.");
+        alert("Operation canceled.");
     }
+
 }
